a59f42271e061eb2e29f9334a51708d166930146
Puspita Vabna <sharif7761@gmail.com>
2021-10-23 20:28:24 +0600

    seeding product and review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,7 +22,11 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'details' => $this->faker->paragraph,
+            'price' => $this->faker->numberBetween(100, 1000),
+            'stock' => $this->faker->randomDigit,
+            'discount' => $this->faker->numberBetween(10, 50),
         ];
     }
 }
