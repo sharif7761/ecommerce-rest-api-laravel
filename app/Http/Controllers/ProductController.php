ce069468685df6030b3d8147c3f9702f8ecdaef0
Puspita Vabna <sharif7761@gmail.com>
2021-10-30 23:19:27 +0600

    update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -87,7 +87,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['details'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
     }
 
     /**
