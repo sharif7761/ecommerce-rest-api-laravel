4189f91f5f9affa4006e350507fff55a702a2921
Puspita Vabna <sharif7761@gmail.com>
2021-11-01 00:33:11 +0600

    store review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,7 +9,11 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
-    
+
+    protected $fillable = [
+        'customer', 'star', 'review'
+    ];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
